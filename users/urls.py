afdc2b5bdc41346074cc3175398a92814b389dfa
dirtTastesGood <keegood8@gmail.com>
2020-08-07 23:08:30 -0700

    add django auth views to users urls and create login.html and signup.html

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'users'
 urlpatterns = [
     path('', views.user_list, name='user_list'),
     path('profile/<int:id>', views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template='login.html'), name='login')
+    path('logout/', auth_views.LogoutView, name='logout' )
 ]
