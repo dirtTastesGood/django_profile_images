3c5dc80c5ee0ef4d0518442752dd725b5dd47237
dirtTastesGood <keegood8@gmail.com>
2020-08-07 17:38:39 -0700

    create ProfileImage model

--- profile_images/models.py
+++ profile_images/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class ProfileImage(models.Model):
+    image = models.ImageField(
+        'profile image',
+        default='profile_images/default.jpg',
+        upload_to='profile_images'
+    )
+
+    def __str__(self):
+        return self.image.name
+
