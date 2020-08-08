48563d6b13167ff2670f63cfe608a6b129507296
dirtTastesGood <keegood8@gmail.com>
2020-08-07 22:53:54 -0700

    linked urls, views and templates

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def user_list(request):
+    return render(request, 'user-list.html') 
+
+def profile(request):
+    return(request, 'profile.html')
+    
