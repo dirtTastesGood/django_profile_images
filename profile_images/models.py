82b80e1b76a551e8e5d548caf650f54f1a32121a
dirtTastesGood <keegood8@gmail.com>
2020-08-09 11:57:26 -0700

    define get_upload_path to dynamically generate file paths for images, added all user profile media folders to gitignore

--- profile_images/models.py
+++ profile_images/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from pathlib import Path
 
 def get_upload_path(instance, filename):
-    pass
+    ''' Calculate and return an upload path for a profile image
+        which includes the user's username '''
+    # get the file's extension 
+    file_extension = filename.split('.')[-1]
+
+    # upload to profile_images/<USERNAME>/
+    file_path = Path(f'profile_images/{instance.customuser.username}/profile_image.{file_extension}') 
+    
+    # file path must be a string
+    return str(file_path)
 
 class ProfileImage(models.Model):
     image = models.ImageField(
         verbose_name='profile image',
         default='default.jpg',
-        upload_to='profile_images'
+        upload_to=get_upload_path,
     )
 
     def __str__(self):
