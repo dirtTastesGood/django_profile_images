fd6df67a3767d96f5a72abab312e8f658fedf659
dirtTastesGood <keegood8@gmail.com>
2020-08-08 21:12:41 -0700

    added comments to signup template and begin processing POST request on signup

--- users/views.py
+++ users/views.py
@@ -4,6 +4,16 @@ from .forms import UserSignupForm
 def signup(request):
     # blank instance of UserCreationForm
     form = UserSignupForm()
+    
+    if request.method == 'POST':
+
+        # create a form with the form data from the request
+        form = UserSignupForm(request.POST)    
+
+
+        if form.is_valid():
+            form.save()
+
 
     # pass to template
     context = {
